71f762e16c05de03c6dc846920304e0148aa001d
jesishen <jesishen@umich.edu>
2024-11-19 22:55:57 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -6,7 +6,9 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video = document.querySelector("#player1");
 	video.autoplay = false;
+		console.log("Auto play off" + video.autoplay);
 	video.loop = false;
+		console.log("Loop off" + video.loop);
 	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 });
 
@@ -16,22 +18,15 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	if (video.paused){
-		video.play();
-		this.innerHTML = "Pause";
-	}else {
-		video.pause()
-		this.innerHTML = "Play";
-	}
+	video.play();
 	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"; // update volume
 });
 
 // Pause the video.
 
 document.querySelector("#pause").addEventListener("click", function(){
-	console.log("pause video");
+	console.log("Pause video");
 	video.pause();
-	document.querySelector("#play").innerHTML = "Play";
 });
 
 // speed up slow down
@@ -39,7 +34,7 @@ document.querySelector("#pause").addEventListener("click", function(){
 // Slow the current video speed by 10% each time the button is clicked and log the new speed to the console.
 
 document.querySelector("#slower").addEventListener("click", function(){
-	console.log("slower clicked")
+	console.log("slower clicked");
 	video.playbackRate *= 0.9;
 	console.log("New speed: " + video.playbackRate);
 });
@@ -48,7 +43,7 @@ document.querySelector("#slower").addEventListener("click", function(){
 // If you slow down three times and then speed up three times you should be within 5 digits of 100% again.
 
 document.querySelector("#faster").addEventListener("click", function(){
-	console.log("faster clicked")
+	console.log("faster clicked");
 	video.playbackRate /= 0.9;
 	console.log("New speed: " + video.playbackRate);
 });
@@ -59,35 +54,30 @@ document.querySelector("#faster").addEventListener("click", function(){
 // Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther. Log the current location of the video.
 
 document.querySelector("#skip").addEventListener("click", function(){
-	console.log("skip clicked")
+	console.log("skip clicked");
 	video.currentTime += 10;
-	if (video.currentTime >= video.duration){
-		video.currentTime = video.currentTime - 0.1;
-	}
 	console.log("Current time is: " + video.currentTime);
 })
 
 // Mute/unmute the video and update the text in the button.
 
 document.querySelector("#mute").addEventListener("click", function(){
-	console.log("mute clicked")
-	if(video.muted){
-		video.muted = false;
-		video.volume = lastVolume;
-		this.innerHTML = "Mute";
-	} else{
-		lastVolume = video.volume;
+	console.log("mute clicked");
+	if (document.getElementById("mute").innerHTML === "Mute"){
 		video.muted = true;
-		this.innerHTML = "Unmute"
+		document.getElementById("mute").innerHTML = "Unmute";
+	} else{
+		video.muted = false;
+		document.getElementById("mute").innerHTML = "Mute";
 	}
 })
 
 // Change the volume based on the slider and update the volume information.
 
 document.querySelector("#slider").addEventListener("input", function(){ // INPUT NOT CLICK OMG
-	console.log("changing")
-	video.volume = this.value / 100;
-	document.querySelector("#volume").innerHTML = this.value * 100 + "%";
+	console.log("volume slider");
+	video.volume = document.querySelector("#slider").value / 100;
+	document.getElementById("#volume").innerHTML = video.volume * 100 + "%";
 })
 
 // GIRL U GOT THIS UR GETTING THE HANG OF IT!! :) <3
@@ -95,13 +85,13 @@ document.querySelector("#slider").addEventListener("input", function(){ // INPUT
 // Utilize the existing oldSchool class on the video element
 
 document.querySelector("#vintage").addEventListener("click", function(){
-	console.log("Vintage vibes")
+	console.log("Vintage vibes");
 	video.classList.add("oldSchool"); // from css
 })
 
 // Remove the oldSchool class from the video.
 
 document.querySelector("#orig").addEventListener("click", function(){
-	console.log("vibes removed")
-	video.classList.remove("oldSchool")
+	console.log("vibes removed");
+	video.classList.remove("oldSchool");
 })
